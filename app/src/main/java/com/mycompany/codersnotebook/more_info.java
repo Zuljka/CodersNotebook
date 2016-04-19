4719307d3bbe64a637e6ecbc1d8ecfef75d20519
Zuljka <mariakazydub@gmail.com>
2016-04-19 19:43:57 +0100

    added emailto option to send feedback

--- app/src/main/java/com/mycompany/codersnotebook/more_info.java
+++ app/src/main/java/com/mycompany/codersnotebook/more_info.java
@@ -1,8 +1,12 @@
 package com.mycompany.codersnotebook;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 public class more_info extends Activity {
 
@@ -16,7 +20,23 @@ public class more_info extends Activity {
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-        getWindow().setLayout((int) (width * 0.6) , (int)(height *0.6));
+        getWindow().setLayout((int) (width *0.9) , (int)(height*0.9));
+
+
+        Button sendEmail;
+
+        sendEmail = (Button) findViewById(R.id.sendEmail);
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", "mariakazydub@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                startActivity(Intent.createChooser(emailIntent, "Contact me by..."));
+            }
+        });
+
     }
 
 }
