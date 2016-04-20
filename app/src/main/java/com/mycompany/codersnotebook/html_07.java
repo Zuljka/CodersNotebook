f5e1eae85ecfa8648a9629ae04366da4dacdb29c
Zuljka <mariakazydub@gmail.com>
2016-04-20 02:24:12 +0100

    started divs

---
+++ app/src/main/java/com/mycompany/codersnotebook/html_07.java
@@ -0,0 +1,14 @@
+package com.mycompany.codersnotebook;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class html_07 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_html_07);
+    }
+
+}
